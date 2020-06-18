5dad41db604b89b9e58c0da0a7cab3dcbb05f185
szxy <406469322@qq.com>
2020-06-18 21:41:57 +0800

    项目初始化

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szxy
+ * Date: 2020/6/18
+ * Time: 21:33
+ */
+echo '<a href="my.php">myphp</a>';
---
+++ my.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szxy
+ * Date: 2020/6/18
+ * Time: 21:35
+ */
+echo 'myphp';
